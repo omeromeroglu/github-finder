6d5a88ace491f84070b90f37c6808feacce54e75
OMER FARUK OMEROGLU <omeromeroglu95@gmail.com>
2022-02-13 22:49:40 +0300

    finished verison

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import { Footer } from "./components/layout/Footer"
 //Pages
 import { Home } from "./pages/Home"
 import { About } from "./pages/About"
 import { NotFound } from "./pages/NotFound"
 //Components
+import { Footer } from "./components/layout/Footer"
 import { Navbar } from "./components/layout/Navbar"
+import  Alert  from "./components/layout/Alert"
+//Context
+import { GithubProvider } from "./context/github/GithubContext"
+import { AlertProvider } from "./context/alert/AlertContext"
+import  User  from "./pages/User"
 
 export default function App() {
   return (
+    <GithubProvider>
+      <AlertProvider>
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
         <main className="container mx-auto px-3 pb-12">
+          <Alert />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/user/:login" element={<User />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
         <Footer/>
       </div>
     </Router>
+    </AlertProvider>
+    </GithubProvider>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { UserResults } from '../components/users/UserResults'
+import  UserSearch  from '../components/users/UserSearch'
 
 export const Home = () => {
 
   return (
       <>
+    <UserSearch/>
    <UserResults/>
    
 
